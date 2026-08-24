1ebd20682712aaa7aa0e9021c267eb3a2b80598f
eggoflex94 <eggermarco@gmx.net>
2026-08-24 15:50:07 +0200

    Refactor Supabase client initialization

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,13 +1,12 @@
+
 import { createClient } from "@supabase/supabase-js";
 
-const url = "https://eqfvhgiyrofjscvimvrc.supabase.co";
+const supabaseUrl = "https://eqfvhgiyrofjscvimvrc.supabase.co";
 
-const key = "sb_publishable_Z0no3Kon-_LxYhEsDdwlrA_u5wU_lIs";
+const supabaseAnonKey =
+  "sb_publishable_Z0no3Kon-_LxYhEsDdwlrA_u5wU_lIs";
 
-export const supabase = createClient(url, key, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true,
-  },
-});
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
