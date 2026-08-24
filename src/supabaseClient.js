da5548cdcfacd0e0aa142a27b9ef583f3251536e
eggoflex94 <eggermarco@gmx.net>
2026-08-24 17:17:04 +0200

    Refactor supabaseClient to export from supabase module

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,10 +1,1 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://eqfvhigyrofjscvimvrc.supabase.co";
-
-const supabaseAnonKey = "sb_publishable_Z0no3Kon-_LxYhEsDdwlrA_u5wU_lIs";
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
+export { supabase } from "./supabase";
