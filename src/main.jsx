3d89b63e61350549b47958a3a130e9c2b0fea855
eggoflex94 <eggermarco@gmx.net>
2026-08-24 11:20:22 +0200

    Refactor React import and render method

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<React.StrictMode><App /></React.StrictMode>);
