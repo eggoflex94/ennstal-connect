cdd41adc25b557890bc65857c97cdbbd83c105fd
eggoflex94 <eggermarco@gmx.net>
2026-08-24 15:52:44 +0200

    Fix supabase URL and anon key formatting

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,10 +1,8 @@
-
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://eqfvhgiyrofjscvimvrc.supabase.co";
+const supabaseUrl = "https://eqfvhigyrofjscvimvrc.supabase.co";
 
-const supabaseAnonKey =
-  "sb_publishable_Z0no3Kon-_LxYhEsDdwlrA_u5wU_lIs";
+const supabaseAnonKey = "sb_publishable_Z0no3Kon-_LxYhEsDdwlrA_u5wU_lIs";
 
 export const supabase = createClient(
   supabaseUrl,
