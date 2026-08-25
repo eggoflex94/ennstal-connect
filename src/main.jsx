becba9b80dfc85b7c22f428c8e9cf1729554f613
eggoflex94 <eggermarco@gmx.net>
2026-08-25 02:06:24 +0200

    Refactor main.jsx for imports and rendering method
    Updated imports and changed CSS file reference from styles.css to app.css. Also modified the rendering method to use React.StrictMode.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+import "./app.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode><App /></React.StrictMode>
 );
