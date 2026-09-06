12e56e1dab23b2c294038903af01cafb0f685023
eggoflex94 <eggermarco@gmx.net>
2026-09-06 05:09:19 +0200

    Load separated admin workspace

--- src/main.jsx
+++ src/main.jsx
@@ -40,6 +40,8 @@ import "./notification-center.css";
 import "./member-onboarding.js";
 import "./member-onboarding.css";
 import "./sidebar-dedupe.js";
+import "./admin-workspace.js";
+import "./admin-workspace.css";
 
 class AppErrorBoundary extends React.Component {
   constructor(props) { super(props); this.state = { failed: false }; }
