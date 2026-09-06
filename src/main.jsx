818c95deeca12b9d9dccf98c371ad7d68d063671
eggoflex94 <eggermarco@gmx.net>
2026-09-06 12:55:50 +0200

    Load final responsive sidebar layout layer

--- src/main.jsx
+++ src/main.jsx
@@ -49,6 +49,7 @@ import "./admin-compact-enhancements.js";
 import "./admin-compact-enhancements.css";
 import "./sidebar-overview.js";
 import "./sidebar-overview.css";
+import "./sidebar-overview-final.css";
 
 class AppErrorBoundary extends React.Component {
   constructor(props) { super(props); this.state = { failed: false }; }
