aa079e12b1c9a0ba54778e70054bc87a0b1a92b9
eggoflex94 <eggermarco@gmx.net>
2026-09-09 01:12:02 +0200

    Remove obsolete Salzkammergut alias runtime

--- src/main.jsx
+++ src/main.jsx
@@ -31,7 +31,6 @@ import "./admin-compact-enhancements.js";
 import "./sidebar-news-popup.js";
 
 import "./regional-shell.js";
-import "./overregional-region-alias.js";
 import "./navigation-stability.js";
 import "./clean-profile-runtime.js";
 import "./clean-layout.css";
