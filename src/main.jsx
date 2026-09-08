f0bf5e9ce851d8204f5fceb799579aba052959ec
eggoflex94 <eggermarco@gmx.net>
2026-09-09 00:30:25 +0200

    Wire stable single-event shell navigation

--- src/main.jsx
+++ src/main.jsx
@@ -31,6 +31,7 @@ import "./admin-compact-enhancements.js";
 import "./sidebar-news-popup.js";
 
 import "./regional-shell.js";
+import "./navigation-stability.js";
 import "./clean-profile-runtime.js";
 import "./clean-layout.css";
 import "./clean-components.css";
