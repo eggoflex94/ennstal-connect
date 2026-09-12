683696033fa1e634ef7fa4f9f1592972b6e12eb0
eggoflex94 <eggermarco@gmx.net>
2026-09-12 22:29:33 +0200

    Load profile target compatibility before scoped profile modules

--- src/profile-community-modules.js
+++ src/profile-community-modules.js
@@ -1,6 +1,7 @@
 import './member-profile-final.js';
 import './member-profile-final.css';
 import './role-rpc-routing-fix.js';
+import './profile-target-compat.js';
 import './profile-photo-album.js';
 import './profile-youtube-embed.js';
 import './profile-regional-role-context.js';
