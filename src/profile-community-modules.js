3d25ff18434e156389ea390edc9a3a5ae0325f6e
eggoflex94 <eggermarco@gmx.net>
2026-09-12 22:49:42 +0200

    Load album file selection guard

--- src/profile-community-modules.js
+++ src/profile-community-modules.js
@@ -3,6 +3,7 @@ import './member-profile-final.css';
 import './role-rpc-routing-fix.js';
 import './profile-target-compat.js';
 import './profile-photo-album.js';
+import './profile-photo-album-file-guard.js';
 import './profile-youtube-embed.js';
 import './profile-regional-role-context.js';
 import './neon-green-theme.css';
