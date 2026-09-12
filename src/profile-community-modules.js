b3ef39910f0fb17f6a9ea88e21fd48cf6cafe0f1
eggoflex94 <eggermarco@gmx.net>
2026-09-12 21:52:10 +0200

    Load responsive profile YouTube embeds

--- src/profile-community-modules.js
+++ src/profile-community-modules.js
@@ -2,6 +2,7 @@ import './member-profile-final.js';
 import './member-profile-final.css';
 import './role-rpc-routing-fix.js';
 import './profile-photo-album.js';
+import './profile-youtube-embed.js';
 
 // The visible member profile is intentionally owned by the agreed final
 // profile authority above. Add only scoped profile modules here.
