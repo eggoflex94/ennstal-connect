c64ef2259bd6ef82bcc09afbd51130b2de39b1da
eggoflex94 <connectennstal@gmail.com>
2026-09-22 13:38:23 +0200

    Stop hijacking native navigation clicks

--- src/ui-requested-fixes.js
+++ src/ui-requested-fixes.js
@@ -6,7 +6,6 @@ import './head-admin-operator-label.js';
 import './role-star-authority.css';
 import './profile-role-star-runtime.js';
 import './global-admin-labels.js';
-import './react-navigation-bridge.js';
 import './head-admin-dock-cleanup.js';
 import './profile-all-groups.css';
 import './profile-all-groups.js';
